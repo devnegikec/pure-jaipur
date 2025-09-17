7027470c93982790161c28347d4b0ffe5e9affed
Devendra Negi <devnegi@Devendras-MacBook-Air.local>
2025-09-17 10:23:31 +0530

    api gateway and authservice

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,14 +1,46 @@
 import express from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+// import swaggerUi from 'swagger-ui-express';
+// import axios from 'axios';
+import cookieParser from 'cookie-parser';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.use(
+  cors({
+    origin: ['http://localhost:6001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
+ 
+app.use(morgan('dev'));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(cookieParser());
+app.set('trust proxy', 1); // trust first proxy
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: any) => (req.user ? 1000: 100),
+  message: {error: 'Too many requests from this IP, please try again later.'},
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: any) => req.ip,
 });
 
+app.use(limiter);
+app.get('/health', (req, res) => res.send({
+  message: 'Welcome to API Gateway!'
+}));
+app.use('/', proxy('http://localhost:6001'));
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
